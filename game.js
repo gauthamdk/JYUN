52126638a37aa6440fe4c528a9b53a9c2f695b4d
Gautham <gauthamkumar.gk.gk@gmail.com>
2021-11-11 10:38:15 -0500

    get username

--- game.js
+++ game.js
@@ -1,6 +1,17 @@
-const player_name = "";
+let player_name = "";
 const items = [];
 
 let gameOver = false;
 
-const getUsername = () => {};
+const getUsername = () => {
+  $("#username_textbox").keydown((e) => {
+    player_name = e.target.value;
+
+    if (e.keyCode == 13) {
+      $("#username").addClass("d-none");
+      $("#gameplay_text").removeClass("d-none");
+    }
+  });
+};
+
+getUsername();
