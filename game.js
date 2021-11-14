1e31ea7cb5989b21de4828dd8d487825ce85c2a7
Gautham <gauthamkumar.gk.gk@gmail.com>
2021-11-14 13:38:41 -0500

    resize for new image sizes

--- game.js
+++ game.js
@@ -41,12 +41,24 @@ const muteDiggingSound = () => {
   $("#digging").prop("muted", true);
 };
 
+const showSlide = (image, text) => {
+  $("#charpd").attr("src", image);
+  $("#charpd").removeClass("d-none");
+  $("#gameplay_text").text(text);
+  $("#gameplay_text").removeClass("d-none");
+};
+
 const gameScene = (p) => {
   console.log(p);
   console.log(player_name);
   if (p == 0) {
   } else if (p == 1) {
     playDiggingSound();
+  } else if (p == 2) {
+    showSlide(
+      "assets/player/CharPD_01.png",
+      "That's not a great sound to hear in a cemetery"
+    );
   }
 };
 
